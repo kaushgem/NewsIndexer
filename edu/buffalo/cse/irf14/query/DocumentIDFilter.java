3b25535149dee22eabb89f5b386645bc2d77b2fb
Sathish <sdeivasi@buffalo.edu>
2014-10-18 15:17:57 -0400

    minor bug fix in for loop

--- edu/buffalo/cse/irf14/query/DocumentIDFilter.java
+++ edu/buffalo/cse/irf14/query/DocumentIDFilter.java
@@ -8,16 +8,14 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 
 import edu.buffalo.cse.irf14.index.IndexType;
-import edu.buffalo.cse.irf14.index.IndexWriter;
+import edu.buffalo.cse.irf14.index.IndicesDTO;
+import edu.buffalo.cse.util.Utility;
 
 /**
- * @author kaush
- * 
+ * @author Kaushik
+ *
  */
 public class DocumentIDFilter {
-<<<<<<< HEAD
-	
-=======
 
 	IndicesDTO indices;
 
@@ -26,7 +24,6 @@ public class DocumentIDFilter {
 	}
 
 
->>>>>>> branch 'master' of https://sizekumar@bitbucket.org/o1/startercode.git
 	/**
 	 * @param type 
 	 * @param term
@@ -40,20 +37,20 @@ public class DocumentIDFilter {
 		{
 			switch (type) {
 			case TERM:
-				if(null!=IndexWriter.termIndex.get(term))
-					docIDList = generateDocIDArray(IndexWriter.termIndex.get(term));
+				if(null!=indices.termIndex.get(term))
+					docIDList = generateDocIDArray(indices.termIndex.get(term));
 				break;
 			case CATEGORY:
-				if(null!=IndexWriter.categoryIndex.get(term))
-					docIDList = generateDocIDArray(IndexWriter.categoryIndex.get(term));
+				if(null!=indices.categoryIndex.get(term))
+					docIDList = generateDocIDArray(indices.categoryIndex.get(term));
 				break;
 			case AUTHOR:
-				if(null!=IndexWriter.authorIndex.get(term))
-					docIDList = generateDocIDArray(IndexWriter.authorIndex.get(term));
+				if(null!=indices.authorIndex.get(term))
+					docIDList = generateDocIDArray(indices.authorIndex.get(term));
 				break;
 			case PLACE:
-				if(null!=IndexWriter.placeIndex.get(term))
-					docIDList = generateDocIDArray(IndexWriter.placeIndex.get(term));
+				if(null!=indices.placeIndex.get(term))
+					docIDList = generateDocIDArray(indices.placeIndex.get(term));
 				break;
 			default:
 				break;
@@ -61,169 +58,32 @@ public class DocumentIDFilter {
 		}
 		return docIDList;
 	}
-	
-	public ArrayList<Integer> getDocIDArrayListForPhraseQueries(IndexType type, String term) {
-		
-		return null;
-	}
-
-
 
 	
 	
 	
 	private ArrayList<Integer> generateDocIDArray(HashMap<Integer, String> postings) {
-
 		ArrayList<Integer> arrLst = new ArrayList<Integer>();
-
 		for (Entry<Integer, String> eItr : postings.entrySet()) {
 			arrLst.add(eItr.getKey());
 		}
 		return arrLst;
 	}
 
-<<<<<<< HEAD
 
-
-	////////////////////////////////////////////////////////
-
-
-//	public ArrayList<Integer> docIDSearch(IndexType type, String query){
-//
-//		ArrayList<Integer> docIDList = null;
-//
-//		if(null!=term)
-//		{
-//			switch (type) {
-//			case TERM:
-//				if(null!=IndexWriter.termIndex.get(term))
-//					docIDList = generateFwdIndex(IndexWriter.termIndex.get(term));
-//				break;
-//			case CATEGORY:
-//				if(null!=IndexWriter.categoryIndex.get(term))
-//					docIDList = generateFwdIndex(IndexWriter.categoryIndex.get(term));
-//				break;
-//			case AUTHOR:
-//				if(null!=IndexWriter.authorIndex.get(term))
-//					docIDList = generateFwdIndex(IndexWriter.authorIndex.get(term));
-//				break;
-//			case PLACE:
-//				if(null!=IndexWriter.placeIndex.get(term))
-//					docIDList = generateFwdIndex(IndexWriter.placeIndex.get(term));
-//				break;
-//			default:
-//				break;
-//			}
-//		}
-//
-//	}
-//
-//
-//	private ArrayList<Integer> generateFwdIndex(HashMap<Integer, String> postings) {
-//
-//		ArrayList<Integer> arrLst = new ArrayList<Integer>();
-//
-//		for (Entry<Integer, String> eItr : postings.entrySet()) {
-//			arrLst.add(eItr.getKey());
-//		}
-//		return arrLst;
-//	}
-//
-
-
-	//	
-	//	{
-	//		String[] q = query.split(" ");
-	//		
-	//		for(String term:q)
-	//		{
-	//			if(null!=IndexWriter.authorIndex.get(term))
-	//				docIDList = generateDocIDArray(IndexWriter.authorIndex.get(term));
-	//			HashMap<Integer, String> map = new HashMap<Integer, String>();
-	//		}
-	//		
-	//		
-	//		return null;
-	//	}
-
-
-=======
->>>>>>> branch 'master' of https://sizekumar@bitbucket.org/o1/startercode.git
-
-	// term, IndexType
 	
-<<<<<<< HEAD
-	public HashMap<Float, Integer> getRankedDocs(HashMap <String, IndexType> query, ArrayList<Integer> docIDs){
-
-		HashMap<Float, Integer> rankedDocs = new HashMap<Float, Integer>();
-		HashMap<Integer, HashMap<String, Float>> fwdIndex = new HashMap<Integer, HashMap<String, Float>>();
-
-		
-		// Iterate for queryTerm in the Query
-		
-		for (Entry<String, IndexType> queryTerm : query.entrySet()) {
-			String term = queryTerm.getKey();
-			IndexType type = queryTerm.getValue();
-
-			// Get the IndexMap corresponding to the type
-			HashMap<String, HashMap<Integer, String>> indexMap = getIndexMap(type);
-			
-			HashMap<Integer, String> postingsMap = null;
-			if(indexMap.get(term) != null){
-				postingsMap = indexMap.get(term);
-			}
-			
-			// Calculate IDF
-			int totalDocs = IndexWriter.docIDLookup.size();
-			int noOfDocsTermOccurs = postingsMap.size();
-			float idf = (float)(Math.log(totalDocs/noOfDocsTermOccurs));
-
-			for (Entry<Integer, String> mapItr : postingsMap.entrySet()) {
-
-				Integer docID = mapItr.getKey();
-				
-				// Check for only the DocIDs received after Query evaluation
-				if(!docIDs.contains(docID)) { continue; }
-				
-				String[] str = mapItr.getValue().split(":");
-				int tf = Integer.parseInt(str[0]);
-				float weight = tf+idf; //TODO:  change formula
-				HashMap<String, Float> fwdIndexInner = null;
-				
-				if(fwdIndex.get(docID)==null)
-				{
-					fwdIndexInner = new HashMap<String, Float>();
-					fwdIndexInner.put(term, weight);
-				}
-				else
-				{
-					fwdIndexInner = fwdIndex.get(docID);
-					fwdIndexInner.put(term, weight);
-				}
-				
-				fwdIndex.put(docID, fwdIndexInner);
-			}
-		}
-
-		//Using fwdIndex calculate total score => rankedDocs
-		
-		return rankedDocs;
-	}
-
-=======
->>>>>>> branch 'master' of https://sizekumar@bitbucket.org/o1/startercode.git
 
 	private HashMap<String, HashMap<Integer, String>> getIndexMap(IndexType type)
 	{
 		switch (type) {
 		case TERM:
-			return IndexWriter.termIndex;
+			return indices.termIndex;
 		case CATEGORY:
-			return IndexWriter.categoryIndex;
+			return indices.categoryIndex;
 		case AUTHOR:
-			return IndexWriter.authorIndex;
+			return indices.authorIndex;
 		case PLACE:
-			return IndexWriter.placeIndex;
+			return indices.placeIndex;
 		default:
 			return null;
 		}
@@ -231,10 +91,10 @@ public class DocumentIDFilter {
 
 
 
+	public ArrayList<Integer> getDocIDArrayListForPhraseQueries(IndexType type, String query) {
 
+		String[] queryTerms = query.split(" ");
 
-<<<<<<< HEAD
-=======
 		ArrayList<Integer> filteredDocIDs = new ArrayList<Integer>(); 
 		HashMap<Integer, HashMap<String, ArrayList<Integer>>> fwdIndex = new HashMap<Integer, HashMap<String, ArrayList<Integer>>>();
 
@@ -297,8 +157,8 @@ public class DocumentIDFilter {
 			}
 
 			String[] value = new String[maxPosition];
-			for(String str:value)
-				str = "$";
+			for(int m=0; m<value.length ; m++)
+				value[m] = "$";
 
 			for(Entry<String, ArrayList<Integer>> innerItr : fwdIndexInner.entrySet())
 			{
@@ -319,5 +179,4 @@ public class DocumentIDFilter {
 	}
 
 
->>>>>>> branch 'master' of https://sizekumar@bitbucket.org/o1/startercode.git
 }
