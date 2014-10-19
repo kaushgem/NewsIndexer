a05bb3325f84a2a7b67460003cba9b74c417f8fa
Sathish <sdeivasi@buffalo.edu>
2014-10-19 04:53:57 -0400

    for Query mode printing

---
+++ edu/buffalo/cse/irf14/query/QueryModeOutput.java
@@ -0,0 +1,11 @@
+package edu.buffalo.cse.irf14.query;
+
+public class QueryModeOutput {
+	public String 	query;
+	
+	public int resultRank;
+	public String resultTitle;
+	public float resultRelevancy;
+	public String snippet;
+
+}
